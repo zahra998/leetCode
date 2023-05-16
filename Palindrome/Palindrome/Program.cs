032b44f6c7c806553b07082ebbca2e350fd477f2
Zahra Al Bulushi <132909001+zahra998@users.noreply.github.com>
2023-05-16 13:24:22 +0400

    check if the number is palindrome or not

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 2003;
+            string str;
+            //str = num.ToString();
+
+            Console.WriteLine("Enter number : ");
+            str = Console.ReadLine();
+
+            char[] stringArray = str.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+            //Console.WriteLine($"Actual String is : {str} \n");
+            //Console.WriteLine($"Reversed String is : {reversedStr} ");
+
+            if (str == reversedStr)
+            {
+                Console.WriteLine(str + " is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(str + " is not Palindrome");
+            }
+        }
+    }
+}
